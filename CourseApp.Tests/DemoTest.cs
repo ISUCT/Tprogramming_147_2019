178c0e08c05f102f8639f1ca219a02c3061c4e21
DeC0re <vladyusya0712@mail.ru>
2019-11-14 14:33:14 +0300

    lab1.1

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -6,11 +6,11 @@ namespace CourseApp.Tests
     public class DemoTest
     {
         [Theory]
-        [InlineData(2.0, 1.2, 4.2, 0.6)]
+        [InlineData(2.0, 5.2, 4.2, 0.6)]
         public void TestTaskA(double a, double xn, double xk, double dx)
         {
-            var resultA = Program.TaskA(a, xn, xk, dx);
-            Assert.Equal(resultA, new double[0]);
+            var res = Program.TaskA(a, xn, xk, dx);
+            Assert.Equal(res, new double[5]);
         }
         [Theory]
         [InlineData(2.0, 1.2, 4.2, 0.6)]
@@ -21,21 +21,22 @@ namespace CourseApp.Tests
             var result = new int[resultA.Length];
             foreach (var item in resultA)
             {
-                result[k] = (int)Math.Floor(resA[k]);
+                result[k] = (int)Math.Floor(resultA[k]*1000);
                 k++;
             }    
             var exp = new int[] {24, 23, 21, 19, 17};
-            Assert.Equal(result, exp, 3);
+            Assert.Equal(result, exp);
         }
-        [Theory]      
-        public void TestTaskB()
+        [Theory]
+        [InlineData(2.0)]     
+        public void TestTaskB(double a)
         {
             var mass = new double[] { 1.16, 1.32, 1.47, 1.65, 1.93 };
-            var resultB = Program.TaskB(2.0, mass);
-            var expy = new double[] { 0.0250047248573638, 0.0246393618709823, 0.0242470198513219, 0.0237322773817727, 0.0228750269630625}
+            var resultB = Program.TaskB(a, mass);
+            var expy = new double[] { 0.0250047248573638, 0.0246393618709823, 0.0242470198513219, 0.0237322773817727, 0.0228750269630625};
             for (int i = 0; i < 5; i++)
             {
-                Assert.Equal(expy[i], resultB[i], 3);
+                Assert.Equal(expy[i], resultB[i], 3 );
             }
         }        
     }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -8,45 +8,42 @@ namespace CourseApp
         {
             int k = 0;
             var y = new double[5];
-            for (var i = xn; i < xk; i += dx)
-            {
-                y[k] = Math.Pow(Math.Log10(a + i), 2) / Math.Pow(a + i, 2);
-                k++;
-            }
+            for (var i = xn; i < xk; i += dx)            
+                {
+                    y[k] = Math.Pow(Math.Log10(a + i), 2) / Math.Pow(a + i, 2);
+                    k++;
+                }
             return y;
         }
-
         public static double[] TaskB(double a, double[] x)
         {
             var y = new double[x.Length];
             for (var i = 0; i < y.Length; i++)
-            {
-                y[i] = Math.Pow(Math.Log10(a + x[i]), 2) / Math.Pow(a + x[i], 2);
-            }
+                {
+                    y[i] = Math.Pow(Math.Log10(a + x[i]), 2) / Math.Pow(a + x[i], 2);
+                }
             return y;
         }
-
         public static void Main(string[] args)
         {
              double a = 2.0;
              double xn = 1.2;
              double xk = 4.2;
              double dx = 0.6;
-
-            var resSingle = TaskA(a, xn, xk, dx);
+            var resA = TaskA(a, xn, xk, dx);
             Console.WriteLine("Задание А:");
-            foreach (var item in resSingle)
-            {
-                Console.WriteLine($"y = {item}");
-            }
-
+            foreach (var item in resA)
+                {
+                    Console.WriteLine($"y = {item}");
+                }
+                
             var x = new double[] { 1.16, 1.32, 1.47, 1.65, 1.93 };
             var taskBRes = TaskB(a, x);
             Console.WriteLine("Задание В:");
             foreach (var item in taskBRes)
-            {
-                Console.WriteLine($"y1 = {item}");
-            }
+                {
+                    Console.WriteLine($"y1 = {item}");
+                }
             Console.ReadLine();
         }
     }
