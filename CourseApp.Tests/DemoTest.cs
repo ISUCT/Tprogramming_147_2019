1faf0dc1fa3fc33932ab7d21bbdbb53db09da031
DeC0re <vladyusya0712@mail.ru>
2019-11-14 15:07:36 +0300

    lav1.2

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -6,38 +6,11 @@ namespace CourseApp.Tests
     public class DemoTest
     {
         [Theory]
-
         [InlineData(2.0, 5.2, 4.2, 0.6)]
         public void TestTaskA(double a, double xn, double xk, double dx)
         {
             var res = Program.TaskA(a, xn, xk, dx);
             Assert.Equal(res, new double[5]);
-
-        [InlineData(0, 0, 0, double.NaN)]
-        [InlineData(1, 1, 2, 2.5)]
-        [InlineData(-1, 1, 1, 0)]
-        public void TestCalc(double a, double b, double x, double exp)
-        {
-            var res = Program.MyFunction(a, b, x);
-            Assert.Equal(exp, res, 3);
-        }
-
-        [Fact]
-        public void TestNormalA()
-        {
-        }
-
-        [Fact]
-        public void TestNormalB()
-        {
-        }
-
-        [Fact]
-        public void TestZeroLengthB()
-        {
-            var res = Program.TaskB(1, 1, new double[0]);
-            Assert.Empty(res);
-
         }
         [Theory]
         [InlineData(2.0, 1.2, 4.2, 0.6)]
