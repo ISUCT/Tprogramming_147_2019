83fc29ae8e64e8c188896d7f8077bd0980232c18
Anatoliy Tkachev <55225900+Tim713-yyd@users.noreply.github.com>
2019-12-05 09:46:46 +0300

    1A, 1B + tests

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,10 +5,44 @@ namespace CourseApp.Tests
 {
     public class DemoTest
     {
-        [Fact]
-        public void Test1()
+         [Theory]
+        [InlineData(0, 0, double.NaN)]
+        [InlineData(0, 1, double.PositiveInfinity)]
+        [InlineData(1, 2, 1.344)]
+        public void TestCalc(double a, double x, double exp)
         {
-            Assert.True(true);
+            var res = Program.MyFunction(a, x);
+            Assert.Equal(exp, res, 3);
+        }
+
+        [Fact]
+        public void TestNormalA()
+        {
+            var exp = Program.TaskA(2.25, 1.2, 2.7, 0.3);
+            var act = new double[] { 3.0103344159677916, 3.6097497543328076, 6.652145016692401, 16.161815341705136, 47.58859115830112, 164.15211982561777 };
+            for (int i = 0; i < 5; i++)
+            {
+            Assert.Equal(exp[i], act[i], 3);
+            }
+        }
+
+        [Fact]
+        public void TestNormalB()
+        {
+            var x = new double[] { 1.31, 1.39, 1.44, 1.56, 1.92 };
+            var act = Program.TaskB(2, x);
+            var exp = new double[] { 2.8713304537946596, 2.9552006807606412, 3.0576161138202966, 3.4685781677040985, 6.838984170495196 };
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(exp[i], act[i], 3);
+            }
+        }
+
+        [Fact]
+        public void TestIfNoB()
+        {
+            var res = Program.TaskB(0, new double[0]);
+            Assert.Empty(res);
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,9 +4,61 @@ namespace CourseApp
 {
     public class Program
     {
+        public static double MyFunction(double a, double x)
+        {
+            var c = System.Math.Pow(a, System.Math.Pow(x, 2) - 1) - System.Math.Log(System.Math.Pow(x, 2) - 1) + System.Math.Pow(System.Math.Pow(x, 2) - 1, 1.0 / 3);
+            return c;
+        }
+
+        public static double[] TaskA (
+                                     double a,
+                                     double xn,
+                                     double xk,
+                                     double dx)
+        {
+            var steps = (int)Math.Floor((xk - xn) / dx) + 1;
+            var y = new double[steps];
+            var i = 0;
+            for (var x = xn; x < xk; x += dx)
+            {
+                y[i] = MyFunction(a, x);
+                i++;
+            }
+
+            return y;
+        }
+
+        public static double[] TaskB (
+                                 double a,
+                                 double[] x)
+        {
+            var y = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = MyFunction(a, x[i]);
+            }
+
+            return y;
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var taskA = TaskA(2.25, 1.2, 2.7, 0.3);
+            Console.WriteLine(taskA);
+
+            for (var i = 0; i < taskA.Length; i++)
+            {
+                Console.WriteLine($"y={taskA[i]}");
+            }
+
+            var xB = new double[] { 1.31, 1.39, 1.44, 1.56, 1.92 };
+            var taskB = TaskB(2, xB);
+            for (var i = 0; i < xB.Length; i++)
+            {
+                Console.WriteLine($"x={xB[i]} y={taskB[i]}");
+            }
+
             Console.ReadLine();
         }
     }
