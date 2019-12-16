5c988278df12ae8e7680cdbaad20dba4f33693a3
Evgenij_Satyev <e.satyev37@gmail.com>
2019-12-16 08:54:09 +0300

    Add Test + collection

--- CourseApp.Tests/FunctionTest.cs
+++ CourseApp.Tests/FunctionTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CourseApp.Tests
@@ -18,17 +19,20 @@ namespace CourseApp.Tests
         [Fact]
         public void TestNormalA()
         {
-            var action = Function.TaskA(0.11, 0.36, 0.05);
-            var s = new double[] { -0.942599174573112, -0.768989662415832, -0.640224062047432, -0.537938691770828, -0.453755045309719 };
-            Assert.Equal(action[0], s[0], 3);
+            List<double> action = Function.TaskA(0.11, 0.36, 0.05);
+            List<double> s = new List<double>() { -0.942599174573112, -0.768989662415832, -0.640224062047432, -0.537938691770828, -0.453755045309719 };
+            for (var i = 0; i < 5; i++)
+            {
+                Assert.Equal(action[i], s[i], 3);
+            }
         }
 
         [Fact]
         public void TestNormalB()
         {
-            var x = new double[] { 0.2, 0.3, 0.38, 0.43, 0.57 };
+            List<double> x = new List<double> { 0.2, 0.3, 0.38, 0.43, 0.57 };
             var actial = Function.TaskB(x);
-            var exp = new double[] { -0.663479953941618, -0.469395196867133, -0.357994574230452, -0.301819984896032, -0.184040925422074 };
+            List<double> exp = new List<double> { -0.663479953941618, -0.469395196867133, -0.357994574230452, -0.301819984896032, -0.184040925422074 };
             for (var i = 0; i < 5; i++)
             {
                 Assert.Equal(exp[i], actial[i], 3);
@@ -38,7 +42,7 @@ namespace CourseApp.Tests
         [Fact]
         public void TestZeroLengthB()
         {
-            var res = Function.TaskB(new double[0]);
+            var res = Function.TaskB(new List<double>());
             Assert.Empty(res);
         }
     }
