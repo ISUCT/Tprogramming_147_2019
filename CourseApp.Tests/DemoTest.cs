a01a09ccb722e01e1b6b18784ab304215ecb775a
Dmitry Ovsyanik <48712046+Rankaros@users.noreply.github.com>
2019-12-17 10:40:48 +0300

    Dmitrij ovsjanik (#31)
    * Name
    
    * Initial app
    
    * Sample of 1st lab
    
    * Tests samples
    
    * Added actions file
    
    * Fixed run steps (#28)
    
    * Test actions on PR (#30)
    
    * Added tests and sample A|B tasks
    
    * Added class task example, added vscode conf
    
    * City v1.0
    
    * Create blank.yml
    
    * City v2.0
    
    * Accepted Incoming Changes
    
    * Functions v1.0
    
    * FunctionsTest v1.0
    
    * Functions v1.1
    
    * City v1.1
    
    * Переделал на листы. Исправил ошибки
    
    * Листы с функциями и рабочие тесты
    
    * Functions v1.2, Empty Lists, xn > xk, dx > xk - xn
    
    * Fixed Test
    
    * Fixed Build with dotnet
    
    * Changed netcore

---
+++ CourseApp.Tests/DemoTest.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CourseApp.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TestMyFunctionZero()
+        {
+            var res = Functions.MyFunction(0.0, 0.0, 0.0);
+            Assert.Equal(0, res);
+        }
+
+        [Fact]
+        public void TestEmptyListA()
+        {
+            double a = 0.1;
+            double b = 0.5;
+            double xn = 2.15;
+            double xk = 1.37;
+            double dx = 0.25;
+            Assert.Empty(Functions.TaskA(a, b, xn, xk, dx));
+        }
+
+        [Fact]
+        public void TestTaskA()
+        {
+            double a = 0.1;
+            double b = 0.5;
+            double xn = 0.15;
+            double xk = 1.37;
+            double dx = 0.25;
+            List<double> res = Functions.TaskA(a, b, xn, xk, dx);
+            List<double> expy = new List<double> { 2.37712776090628 * Math.Pow(10, -05), 0.00022580635007, 0.00090281492133, 0.00270370995817, 0.00689161993993 };
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(expy[i], res[i], 3);
+            }
+        }
+
+        [Fact]
+        public void TestXnMoreThenXk()
+        {
+            List<double> res = Functions.TaskA(0.1, 0.5, 1.37, 0.15, 0.25);
+            Assert.Equal(res, new List<double>());
+        }
+
+        [Fact]
+        public void TestDx()
+        {
+            List<double> res = Functions.TaskA(0.1, 0.5, 1.37, 0.15, 2.0);
+            Assert.Equal(res, new List<double>());
+        }
+
+        [Fact]
+        public void TestEmptyListB()
+        {
+            List<double> x = new List<double>();
+                Assert.Empty(Functions.TaskB(0.1, 0.5, x));
+        }
+
+        [Fact]
+        public void TestTaskB()
+        {
+            List<double> x = new List<double> { 0.2, 0.3, 0.44, 0.6, 0.56 };
+            List<double> res = Functions.TaskB(0.1, 0.5, x);
+            List<double> expy = new List<double> { 4.40297534162119 * Math.Pow(10, -05), 0.000110574248889246, 0.000290504593574433, 0.000704902427233617, 0.000573207189723587 };
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(expy[i], res[i], 3);
+            }
+        }
+    }
+}
---
+++ CourseApp/Functions.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+
+namespace CourseApp
+{
+    public class Functions
+    {
+        public static double MyFunction(double a, double b, double x)
+        {
+            var y = (a + Math.Pow(Math.Tan(b * x), 2)) / (b + (1 / Math.Pow(Math.Tan(a * x), 2)));
+            return y;
+        }
+
+        public static List<double> TaskA (
+                                     double a,
+                                     double b,
+                                     double xn,
+                                     double xk,
+                                     double dx)
+        {
+            if (dx > (xk - xn))
+            {
+                List<double> y = new List<double>();
+                return y;
+            }
+
+            if (xn < xk)
+            {
+                List<double> y = new List<double>();
+                for (double x = xn; x < xk; x += dx)
+                {
+                    y.Add(MyFunction(a, b, x));
+                }
+
+                return y;
+            }
+            else
+            {
+                List<double> y = new List<double>();
+                return y;
+            }
+        }
+
+        public static List<double> TaskB (
+                                 double a,
+                                 double b,
+                                 List<double> x)
+        {
+            List<double> y = new List<double>();
+            for (var i = 0; i < x.Count; i++)
+            {
+                y.Add(MyFunction(a, b, x[i]));
+            }
+
+            return y;
+        }
+
+        public static void Main(string[] args)
+        {
+            const double a = 0.1;
+            const double b = 0.5;
+            const double xn = 0.15;
+            const double xk = 1.37;
+            const double dx = 0.25;
+            Console.WriteLine("TaskA:");
+            foreach (var item in TaskA(a, b, xn, xk, dx))
+            {
+                Console.WriteLine($"y={item}");
+            }
+
+            Console.WriteLine("TaskB:");
+            List<double> x = new List<double> { 0.2, 0.3, 0.44, 0.6, 0.56 };
+            foreach (var item in TaskB(a, b, x))
+            {
+                Console.WriteLine($"y = {item}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
