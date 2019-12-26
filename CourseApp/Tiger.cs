ba7fb0b5b01ef5b4fde9fe987a00999600d69946
Enik3 <48732427+Enik3@users.noreply.github.com>
2019-12-26 10:24:32 +0400

    Aleksandr enivatov (#44)
    * Sample of 1st lab
    
    * Tests samples
    
    * Added actions file
    
    * Fixed run steps (#28)
    
    * Test actions on PR (#30)
    
    * Added tests and sample A|B tasks
    
    * Added class task example, added vscode conf
    
    * List's one lab
    
    * 1 lab
    
    * Klass
    
    * klass2
    
    * klass3
    
    * Superkass
    
    * SuperKlass
    
    * super klas2
    
    * super klass3
    
    Co-authored-by: Eugeny Konstantinov <jskonst@yandex.ru>

---
+++ CourseApp/Tiger.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+
+namespace CourseApp
+{
+    public class Tiger : SuperAnimals
+    {
+        private int age;
+        private string sex;
+
+        public Tiger()
+        : this("Test")
+        {
+        }
+
+        public Tiger(string name, int age, string sex)
+        : base(name, age, sex)
+        {
+        }
+
+        public Tiger(string name)
+        : this(name, 1, "male")
+        {
+        }
+
+        public override string Sex
+        {
+            get
+            {
+                return this.sex;
+            }
+
+            set
+            {
+                if (value == "male" || value == "female")
+                {
+                    this.sex = value;
+                }
+                else
+                {
+                    throw new System.Exception();
+                }
+            }
+        }
+
+        public override int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value >= 1 && value < 20)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new System.Exception();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Tiger(base) ToString";
+        }
+
+        public override void AgeUp()
+        {
+            this.age++;
+        }
+
+        public override string Vose()
+        {
+            return "grrgrr";
+        }
+
+         public override string Runningspeed()
+        {
+            return "50-85km/h";
+        }
+    }
+}
