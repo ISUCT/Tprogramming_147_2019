1542823c94299a17991d89fb98099b497d907ea0
DeC0re <vladyusya0712@mail.ru>
2019-12-31 19:47:40 +0300

    DemoRPG

---
+++ CourseApp/Game.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPG
+{
+    public class Game
+    {
+        public List<Hero> heroes = new List<Hero>();
+        Logger logger = new Logger();
+        Random random = new Random();
+        Hero hero1;
+        Hero hero2;
+        int counter = 0;
+        
+        public void Start(int numOfHeroes)
+        {
+            CreateHeroes(numOfHeroes);
+            while(heroes.Count > 1)
+            {
+                Fight();
+            }
+            
+            Console.WriteLine($"\n=========\nПобедитель в игре - {heroes[0].typeClass} {heroes[0].Name}\n=========");
+        }
+
+        public void CreateHeroes(int numOfHeroes)
+        {
+
+            while(numOfHeroes > 0)
+            {
+                switch(random.Next(0,3))
+                {
+                    case 0: 
+                        heroes.Add(new Archer());
+                        break;
+                    case 1:
+                        heroes.Add(new Knight());
+                        break;
+                    case 2:
+                        heroes.Add(new Wizard());
+                        break;
+                    default:
+                        heroes.Add(new Wizard());
+                        break;
+                }
+                numOfHeroes--;
+            }
+        }
+
+        public void Atack(Hero hero1, Hero hero2)
+        {
+            int damage;
+        
+            if(hero1.buf)
+            {
+                hero1.Atack(out damage);
+                logger.Atack(hero1, hero2, damage);
+                hero2.GetDamage(damage + 5);
+            }
+            else
+            {    
+                hero1.Atack(out damage);
+                logger.Atack(hero1, hero2, damage);
+                hero2.GetDamage(damage);
+            }
+        }
+
+        public void UseSkill(Hero hero1, Hero hero2)
+        {
+            int damage;
+            string skillName;
+            
+            if(hero1.buf)
+            {
+                hero1.Skill(out skillName, out damage);
+                logger.Skill(hero1, hero2, skillName, damage);
+                hero2.GetDamage(damage + 5);
+            }
+            else
+            {    
+                hero1.Skill(out skillName, out damage);
+                logger.Skill(hero1, hero2, skillName, damage);
+                hero2.GetDamage(damage);
+            }
+        }
+
+        public void Fight()
+        {
+            int i = random.Next(0, heroes.Count - 1);
+            hero1 = heroes[i];
+            heroes.RemoveAt(i);
+
+            i = random.Next(0, heroes.Count);
+            hero2 = heroes[i];
+            heroes.RemoveAt(i);
+
+            int turn = random.Next(0, 1);
+            counter++;
+            Console.WriteLine("=========");
+            Console.WriteLine($"Бой №{counter}");
+
+            while (true)
+            {
+                if(turn == 0)
+                {
+                    if(hero2.sleepTime == 0)
+                    {
+                        if(random.Next(0,10) > 6)
+                        {
+                            UseSkill(hero1, hero2);
+                        }
+                        else
+                        {
+                            Atack(hero1, hero2);
+                        }
+                    }
+                    else
+                    {
+                        logger.Sleep(hero1);
+                        hero2.sleepTime--;
+                    }
+
+                    if(hero2.Health <= 0)
+                    {
+                        hero1.sleepTime = 0;
+                        hero1.buf = false;
+                        heroes.Add(hero1);
+                        logger.Winner(hero1);
+                        logger.Death(hero2);
+                        break;
+                    }
+
+                    turn = 1;
+                }
+                else
+                {
+                    if(hero1.sleepTime == 0)
+                    {
+                        if(random.Next(0,10) > 6)
+                        {
+                            UseSkill(hero2, hero1);
+                        }
+                        else
+                        {
+                            Atack(hero2, hero1);
+                        }
+                    }
+                    else
+                    {
+                        logger.Sleep(hero2);
+                        hero1.sleepTime--;
+                    }
+
+                    if(hero1.Health <= 0)
+                    {
+                        hero2.sleepTime = 0;
+                        hero2.buf = false;
+                        heroes.Add(hero2);
+                        logger.Winner(hero2);
+                        logger.Death(hero1);
+                        break;
+                    }
+
+                    turn = 0;
+                }
+            }
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,75 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CourseApp
+namespace RPG
 {
-    public class Program
+    class Program
     {
-        public static double Function(double a, double x)
+        static void Main(string[] args)
         {
-            var y = Math.Pow(Math.Log10(a + x), 2) / Math.Pow(a + x, 2);
+            Game game = new Game();
+            Console.Write("Введите число героев: ");
+            
+            int numOfHeroes = Int32.Parse(Console.ReadLine());
 
-            return y;
-        }
+            if(numOfHeroes % 2 != 0)
+            {
+                numOfHeroes += 1;
+            }
 
-        public static List<double> TaskA(double a, double xn, double xk, double dx)
-        {
-            int k = 0;
-            List<double> y = new List<double>(5);
-            for (var i = xn; i < xk; i += dx)
-                {
-                    y.Add(Function(a, i));
-                    k++;
-                }
+            game.Start(numOfHeroes);
 
-            return y;
-        }
-
-        public static List<double> TaskB(double a, List<double> x)
-        {
-            List<double> y = new List<double>(5);
-            for (var i = 0; i < x.Count; i++)
-                {
-                    y.Add(Function(a, x[i]));
-                }
-
-            return y;
-        }
-
-        public static void Main(string[] args)
-        {
-             double a = 2.0;
-             double xn = 1.2;
-             double xk = 4.2;
-             double dx = 0.6;
-            var resA = TaskA(a, xn, xk, dx);
-            Console.WriteLine("==========");
-            Console.WriteLine("Задание А:");
-            Console.WriteLine("==========");
-            foreach (var item in resA)
-                {
-                    Console.WriteLine($"y = {item}");
-                }
-
-            Console.WriteLine("==========");
-            List<double> x = new List<double> { 1.16, 1.32, 1.47, 1.65, 1.93 };
-            var taskBRes = TaskB(a, x);
-            Console.WriteLine("Задание В:");
-            Console.WriteLine("==========");
-            foreach (var item in taskBRes)
-                {
-                    Console.WriteLine($"y1 = {item}");
-                }
-
-            Console.WriteLine("==========");
-            Console.WriteLine("Введите год своего рождения:");
-            int years = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите месяц своего рождения:");
-            int months = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите день своего рождения:");
-            int days = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(AgeClass.Age(days, months, years));
+            Console.ReadKey();
         }
     }
 }
