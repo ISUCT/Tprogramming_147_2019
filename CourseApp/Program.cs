b539586a812854c10efa6326f387f57de98e32f9
DeC0re <vladyusya0712@mail.ru>
2019-12-31 19:06:56 +0300

    RGP game

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,75 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CourseApp
+namespace RPG
 {
-    public class Program
+    class Program
     {
-        public static double Function(double a, double x)
+        static void Main(string[] args)
         {
-            var y = Math.Pow(Math.Log10(a + x), 2) / Math.Pow(a + x, 2);
+            Game game = new Game();
+            Console.Write("Введите число героев: ");
+            
+            int numOfHeroes = Int32.Parse(Console.ReadLine());
 
-            return y;
-        }
-
-        public static List<double> TaskA(double a, double xn, double xk, double dx)
-        {
-            int k = 0;
-            List<double> y = new List<double>(5);
-            for (var i = xn; i < xk; i += dx)
-                {
-                    y.Add(Function(a, i));
-                    k++;
-                }
-
-            return y;
-        }
-
-        public static List<double> TaskB(double a, List<double> x)
-        {
-            List<double> y = new List<double>(5);
-            for (var i = 0; i < x.Count; i++)
-                {
-                    y.Add(Function(a, x[i]));
-                }
-
-            return y;
-        }
-
-        public static void Main(string[] args)
-        {
-             double a = 2.0;
-             double xn = 1.2;
-             double xk = 4.2;
-             double dx = 0.6;
-            var resA = TaskA(a, xn, xk, dx);
-            Console.WriteLine("==========");
-            Console.WriteLine("Задание А:");
-            Console.WriteLine("==========");
-            foreach (var item in resA)
-                {
-                    Console.WriteLine($"y = {item}");
-                }
-
-            Console.WriteLine("==========");
-            List<double> x = new List<double> { 1.16, 1.32, 1.47, 1.65, 1.93 };
-            var taskBRes = TaskB(a, x);
-            Console.WriteLine("Задание В:");
-            Console.WriteLine("==========");
-            foreach (var item in taskBRes)
-                {
-                    Console.WriteLine($"y1 = {item}");
-                }
-
-            Console.WriteLine("==========");
-            Animals[] animals = new Animals[2];
-            animals[0] = new Dog("White", 5, "female");
-            animals[1] = new Cat("Black");
-            foreach (Animals animal in animals)
+            if(numOfHeroes % 2 != 0)
             {
-                animal.Info();
-                Console.WriteLine();
+                numOfHeroes += 1;
             }
+
+            game.Start(numOfHeroes);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CourseApp/Wizard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RPG
+{
+    public class Wizard : Hero
+    {
+        Random random = new Random();
+
+        public Wizard()
+        : base()
+        {
+            typeClass = "Маг";
+            skills = new string[]{"Заворожение","Метеор"};
+        }
+
+        public override void Skill(out string skillName, out int damage)
+        {
+            int probability = random.Next(0, 100);
+            if(probability <= 60 && sleepTime == 0)
+            {
+                skillName = "Заворожение";
+            }
+            else
+            {
+                skillName = "Метеор";
+            }
+
+            switch(skillName)
+            {
+                case "Заворожение":
+                    sleepTime = 2;
+                    damage = 0;
+                    break;
+                case "Метеор":
+                    damage = (int)Math.Floor(Strength * 2.5);
+                    break;
+                default:
+                    damage = 0;
+                    break;
+            }
+        }
+    }
+}
