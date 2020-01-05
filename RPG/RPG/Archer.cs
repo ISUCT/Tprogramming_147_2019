4f4e4086c20d21636cde830e34da4c25b3b0fc5c
DeC0re <vladyusya0712@mail.ru>
2020-01-05 14:10:03 +0300

    DemoRPG

---
+++ RPG/RPG/Archer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RPG
+{
+    public class Archer : Hero
+    {
+        Random random = new Random();
+
+        public Archer()
+        : base()
+        {
+            typeClass = "Лучник";
+            skills = new string[]{"Огненные стрелы", "Град стрел", "Мощный выстрел"};
+        }
+
+        public override void Skill(out string skillName, out int damage)
+        {
+            int probability = random.Next(0, 100);
+            if(!buf)
+            {
+                if(probability <= 20)
+                {
+                    skillName = "Мощный выстрел";
+                }
+                else if(probability <= 90)
+                {
+                    skillName = "Огненные стрелы";
+                }
+                else
+                {
+                    skillName = "Град стрел";
+                }
+            }
+            else
+            {
+                if(probability <= 70)
+                {
+                    skillName = "Мощный выстрел";
+                }
+                else
+                {
+                    skillName = "Град стрел";
+                }
+            }
+
+            switch(skillName)
+            {
+                case "Мощный выстрел":
+                    damage = (int)(Strength * 1.5);
+                    break;
+                case "Град стрел":
+                    damage = (int)(Strength * 2.5);
+                    break;
+                case "Огненные стрелы":
+                    bufName = "Огненные стрелы";
+                    buf = true;
+                    damage = 0;
+                    break;
+                default:
+                    damage = 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ RPG/RPG/Logger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace RPG
+{
+    class Logger
+    {
+        public void Atack(Hero hero1, Hero hero2, int damage)
+        {
+            if(hero1.typeClass == "Рыцарь")
+            {
+                Console.Write($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) ударил мечом {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) и нанёс {damage} урона.");
+            } 
+            else if(hero1.typeClass == "Лучник")
+            {
+                Console.Write($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) выстрелил в {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) и нанёс {damage} урона.");
+            } 
+            else
+            {
+                Console.Write($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) ударил посохом {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) и нанёс {damage} урона.");
+            }
+            
+            if(hero1.buf)
+            {
+                Console.WriteLine($" Дополнительный урон 5 единиц из-за усиления \"{hero1.bufName}\".");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        public void Skill(Hero hero1, Hero hero2, string skillName, int damage)
+        {
+            if(hero1.sleepTime > 0 && damage == 0)
+            {
+                Console.WriteLine($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) использует умение \"{skillName}\" и оглушает {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) на {hero1.sleepTime} хода.");
+            }
+            else if(hero1.sleepTime > 1)
+            {
+                Console.WriteLine($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) использовал(-a) умение \"{skillName}\" и нанес(-ла) {hero2.typeClass} {hero2.Name} {damage} урона. {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) оглушен(-а) на {hero1.sleepTime} ход(-а).");
+            }
+            else if(hero1.buf && damage == 0)
+            {
+                Console.WriteLine($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) активировал усиление \"{skillName}\"");
+            }
+            else
+            {
+                Console.Write($"{hero1.typeClass} {hero1.Name} ({hero1.Health} / {hero1.maxHealth}) использовал(-a) умение \"{skillName}\" и нанес(-ла) {hero2.typeClass} {hero2.Name} ({hero2.Health} / {hero2.maxHealth}) {damage} урона.");
+                
+                if(hero1.buf)
+                {
+                    Console.WriteLine($" Дополнительный урон 5 единиц из-за усиления \"{hero1.bufName}\".");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void Sleep(Hero hero)
+        {
+            Console.WriteLine($"{hero.typeClass} {hero.Name} ({hero.Health} / {hero.maxHealth}) оглушен(-а) и пропускает ход.");
+        }
+
+        public void Winner(Hero hero)
+        {
+            Console.WriteLine($"{hero.typeClass} {hero.Name} ({hero.Health} / {hero.maxHealth}) победил(-а)!");
+        }
+
+        public void Death(Hero hero)
+        {
+            Console.WriteLine($"{hero.typeClass} {hero.Name} погибает");
+        }
+    }
+}
