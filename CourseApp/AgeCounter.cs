eeb052a2bb6c4fe0306e1c23d5cc24fa56a20544
Dmitry-ops <52451095+Dmitry-ops@users.noreply.github.com>
2020-01-09 18:51:42 +0300

    fix

--- CourseApp/AgeCounter.cs
+++ CourseApp/AgeCounter.cs
@@ -8,23 +8,9 @@ namespace CourseApp
 
         public string CountAge(int day, int month, int year)
         {
-            var birthday = new DateTime(year, month, day);
-            var today = DateTime.Today;
-
-            if (birthday.Ticks > now.Ticks)
-            {
-                throw new Exception("плохая дата;)");
-            }
-            else if (birthday.Ticks == now.Ticks)
-            {
-                throw new Exception("нельзя вводить текущую дату");
-            }
-            else
-            {
-                var age = new DateTime(today.Ticks - birthday.Ticks);
-
-                return $"Возраст:{age.Year - 1} лет, {age.Month - 1} месяцев, {age.Day - 1} дней";
-            }
+           var today = DateTime.Today;
+            var ageCounter = new AgeCounter();
+            return ageCounter.CountAge(day, month, year, today.Day, today.Month, today.Year);
         }
 
         public string CountAge(int day, int month, int year, int nowDay, int nowMonth, int nowYear)
