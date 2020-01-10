d80e2da2ccc99715bf6a8fdf650d1317c48bae7a
Veniamin1511 <kolmogorow2@mail.ru>
2020-01-10 21:46:36 +0300

    Update Pig.cs

--- CourseApp/Pig.cs
+++ CourseApp/Pig.cs
@@ -1,56 +1,225 @@
 using System;
 
+using System.Collections.Generic;
+
+
+
 namespace CourseApp
+
 {
-    public class City
+
+    public class Pig
+
     {
-        private int pop;
 
-        public City()
-        : this(0, "Unitiled", "Unnamed")
-        {
-        }
+        private int salo;
 
-        public City(int pop, string country, string name)
-        {
-            Pop = pop;
-            Country = country;
-            Name = name;
-        }
+        private int age;
 
-        public string Country { get; set; }
+        private string gender;
 
         public string Name { get; set; }
 
-        public int Pop
+        public Pig()
+
+        : this("Test")
+
         {
+
+        }
+
+
+
+        public Pig(string name)
+
+        : this(name, 1,"male")
+
+        {
+
+        }
+
+
+
+        public Pig(string name, int age, string gender)
+
+        : this(name, age, 1, "male")
+
+        {
+
+        }
+
+        
+
+        public Pig(string name, int age, int salo, string gender)
+
+        {
+
+            Name = name;
+
+            Age = age;
+
+            Salo = salo;
+
+            Gender = gender;
+
+        }
+
+
+
+        public int Age
+
+        {
+
             get
+
             {
-                return this.pop;
+
+                return this.age;
+
             }
 
             set
+
             {
-                if (value >= 0)
+
+                if (value >= 1 && value <= 15)
+
                 {
-                    this.pop = value;
+
+                    this.age = value;
+
                 }
+
                 else
+
                 {
-                    Console.WriteLine("Population should be > 0.");
+
+                    Console.WriteLine("Возраст свиньи должен быть менее 15.");
+
                 }
+
             }
+
         }
 
-        public string View()
+        public string Gender
+
         {
-        return @"
-▐▌▐▐▄▄▄▐███▌▄███▄▐▌▌
-▐██▐█▄█▐▌▌▐▌█▄█▄█▐▌█▌
-▐▄██▄█▐▐▌▌▐▌█▄█▄█▐▌▌█▌
-▐█▄██▐▐▐▌▌▐▌█▄█▄█▐▌▌▌█
-██████████████████████
-";
+
+            get
+
+            {
+
+                return this.gender;
+
+            }
+
+            set
+
+            {
+
+                if (value == "male" || value == "female")
+
+                {
+
+                    this.gender = value;
+
+                }
+
+                else
+
+                {
+
+                    throw new System.Exception();
+
+                }
+
+            }
+
         }
+
+        public int Salo
+
+        {
+
+            get
+
+            {
+
+                return this.salo;
+
+            }
+
+
+
+            set
+
+            {
+
+                if (value >= 0)
+
+                {
+
+                    this.salo = value;
+
+                }
+
+                else
+
+                {
+
+                throw new System.Exception();
+
+                }
+
+            }
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return $"Имя:{Name},Возраст:{Age},Cало:{Salo}";
+
+            return @"       _       
+
+      (_)      
+
+ _ __  _  __ _ 
+
+| '_ \| |/ _` |
+
+| |_) | | (_| |
+
+| .__/|_|\__, |
+
+| |       __/ |
+
+|_|      |___/ 
+
+";
+
+        }
+
+        public void Aging()
+
+        {
+
+            this.age++;
+
+        }
+
+        public void LostSalo()
+
+        {
+
+            this.salo--;
+
+        }
+
     }
+
 }
