29f89fd23c78768095038b9f43d5c7b50f386721
Veniamin1511 <kolmogorow2@mail.ru>
2020-01-10 21:47:18 +0300

    Update PigTest.cs

--- CourseApp.Tests/PigTest.cs
+++ CourseApp.Tests/PigTest.cs
@@ -1,57 +1,107 @@
 using System;
+
 using Xunit;
 
+
+
 namespace CourseApp.Tests
+
 {
-    public class CityTest
+
+    public class PigTest
+
     {
+
         [Fact]
+
         public void TestEmptyConstructor()
+
         {
-            var item = new City();
-            Assert.Equal(0, item.Pop);
-            Assert.Equal("Unitiled", item.Country);
-            Assert.Equal("Unnamed", item.Name);
+
+            var item = new Pig();
+
+            Assert.Equal(1, item.Age);
+
+            Assert.Equal("Test", item.Name);
+
+            Assert.Equal(1, item.Salo);
+
+            Assert.Equal("male", item.Gender);
+
         }
+
+
+
+        [Theory]
+
+        [InlineData("PigMan", 5, 7,"male")]
+
+        [InlineData("PigGirl", 2, 1,"female")]
+
+        [InlineData("PigPigger", 10, 11,"male")]
+
+        public void TestFullConstructor(string name, int age, int salo, string gender)
+
+        {
+
+            var item = new Pig(name, age, salo, gender);
+
+            Assert.Equal(age, item.Age);
+
+            Assert.Equal(name, item.Name);
+
+            Assert.Equal(salo, item.Salo);
+
+            Assert.Equal(gender, item.Gender);
+
+        }
+
+
 
         [Fact]
-        public void TestView()
+
+        public void TestRightSetAge()
+
         {
-            var item = new City();
-            var view = @"
-▐▌▐▐▄▄▄▐███▌▄███▄▐▌▌
-▐██▐█▄█▐▌▌▐▌█▄█▄█▐▌█▌
-▐▄██▄█▐▐▌▌▐▌█▄█▄█▐▌▌█▌
-▐█▄██▐▐▐▌▌▐▌█▄█▄█▐▌▌▌█
-██████████████████████
-";
-            Assert.Equal(view, item.View());
+
+            var item = new Pig();
+
+            item.Age = 11;
+
+            Assert.Equal(11, item.Age);
+
         }
+
+
 
         [Fact]
 
-        public void TestSetPop()
+        public void TestWrongSetAge()
+
         {
-            var item = new City();
-            item.Pop = 10;
-            Assert.Equal(10, item.Pop);
+
+            try
+
+            {
+
+                var item = new Pig();
+
+                item.Age = -1000;
+
+            }
+
+            catch (System.Exception)
+
+            {
+
+                Console.WriteLine("Возраст должен быть больше 1 года");
+
+                Assert.True(true);
+
+            }       
+
         }
 
-        [Fact]
-        public void TestINcorrectSetPop()
-        {
-            var item = new City();
-            item.Pop = -100000;
-            Assert.Equal(0, item.Pop);
-        }
-
-        [Fact]
-        public void TestCorrectIncorrectSetPop()
-        {
-            var item = new City();
-            item.Pop = 1;
-            item.Pop = -100000;
-            Assert.Equal(1, item.Pop);
-        }
     }
+
 }
