a58eda8e7586b43bbd42c390c9653226bd6da193
Viktor Morohovsev <171007030408fbd@gmail.com>
2020-03-30 16:26:29 +0300

    without 2 last age tests

--- CourseApp/AgeClass.cs
+++ CourseApp/AgeClass.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace CourseApp
 {
@@ -8,18 +7,22 @@ namespace CourseApp
         public static string Age(int days, int months, int years)
         {
             DateTime result = DateCompare(new DateTime(years, months, days), DateTime.Now);
-            return $"Вам {result.Year - 1} лет, {result.Month - 1} месяцев и {result.Day - 1} дня";
+            return $"Вам {result.Year - 1} лет, {result.Month - 1} месяцев и {result.Day - 2} дня";
         }
 
         public static DateTime DateCompare(DateTime date1, DateTime date2)
         {
             if (date1.Ticks < date2.Ticks)
             {
-                DateTime res = new DateTime(date2.Ticks - date1.Ticks);
-                return res;
+                return new DateTime(date2.Ticks - date1.Ticks);
             }
 
-            throw new Exception();
+            if (date1.Date == date2.Date)
+            {
+                throw new Exception("Понимаю, только родился");
+            }
+
+            throw new Exception("Добро пожаловать в прошлое");
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,84 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 
 namespace CourseApp
 {
     public class Program
     {
-        public static double Formula(double a, double x)
+        public static void ConsoleInput()
         {
-            var y = Math.Pow(a, Math.Pow(x, 2) - 1) - Math.Log10(Math.Pow(x, 2) - 1) + Math.Pow(Math.Sqrt(Math.Pow(x, 2) - 1), 1 / 3);
-            return y;
-        }
-
-        public static List<double> TaskA(double a, double xn, double xk, double dx)
-        {
-            if (xk < xn)
-            {
-                return new List<double>();
-            }
-            else
-            {
-                List<double> y = new List<double>((int)((xk - xn) / dx));
-                for (double x = xn; x < xk; x += dx)
-                {
-                    y.Add(Formula(a, x));
-                }
-
-                return y;
-            }
-        }
-
-        public static List<double> TaskB(double a, List<double> x)
-        {
-            List<double> y = new List<double>();
-            for (var i = 0; i < x.Count; i++)
-            {
-                y.Add(Formula(a, x[i]));
-            }
-
-            return y;
-        }
-
-        public static void Main(string[] args)
-        {
-            const double a = 1.6;
-            const double xn = 1.2;
-            const double xk = 3.7;
-            const double dx = 0.5;
-            Console.WriteLine("Task А:");
-            foreach (var item in TaskA(a, xn, xk, dx))
-            {
-                Console.WriteLine($"y = {item}");
-            }
-
-            Console.WriteLine("Task B:");
-            List<double> x = new List<double> { 1.28, 1.36, 2.47, 3.68, 4.56 };
-            foreach (var item in TaskB(a, x))
-            {
-                Console.WriteLine($"y = {item}");
-            }
-
-            Console.WriteLine();
-
-            Person[] masss = new Person[2];
-            masss[0] = new Student("Artem", "Scherbinin", 18, true, true);
-            masss[1] = new Children("Alina", "Kotova", 7, false, true);
-            foreach (var item in masss)
-            {
-                Console.WriteLine(item.ToString());
-                Console.WriteLine(item.Replica());
-                Console.WriteLine();
-            }
-
             Console.WriteLine("Введите год своего рождения:");
             int years = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите месяц своего рождения:");
             int months = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите день своего рождения:");
             int days = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(AgeClass.Age(days, months, years));
-            Console.WriteLine();
+            if (days == 0 && months == 0)
+            {
+                Console.WriteLine(AgeClass.Age(days, months, years));
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ConsoleInput();
             Console.ReadLine();
         }
     }
